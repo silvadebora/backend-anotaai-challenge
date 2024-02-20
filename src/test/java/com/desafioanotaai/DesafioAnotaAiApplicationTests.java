6a59dcc4498d2c4d676cc43e77302628b23bc12e
Debora <silvadeborafisio@gmail.com>
2024-02-20 16:25:26 -0300

    initial commit

---
+++ src/test/java/com/desafioanotaai/DesafioAnotaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioanotaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAnotaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
